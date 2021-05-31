b081a999b8a91a0eac7231fa406d33febf6711f8
dannnyliang <youchenliang@gmail.com>
2021-05-31 15:53:33 +0800

    refactor: clean up legacy files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-import App from "./App";
-
-test("renders title", () => {
-  render(<App />);
-  const title = screen.getByText(/hello world/i);
-  expect(title).toBeInTheDocument();
-});
